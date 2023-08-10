e9f9f48173f069849e71faefaa73a3c39c60449d
Jake Doan <jakedoan3@gmail.com>
2023-08-10 09:54:37 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/Home";
 import { auth } from "./firebase";
 import PrivateRoutes from "./utils/PrivateRoutes";
 
-//last updated August 8, 2023
+//last updated August 10, 2023
 
 //TO DO:
 //keep authenticated users logged in with cookies/auth context
